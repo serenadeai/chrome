8214d0505fc306dab5ccb7587deaa1e2cff5d1f6
cheng <ummcheng@gmail.com>
2020-04-27 13:03:15 -0700

    add newlines

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,9 @@ import BaseCommandHandler from "./shared/command-handler";
 chrome.runtime.onInstalled.addListener(() => {
   const commandHandler = new CommandHandler();
   // TODO: Our CommandHandler needs to eventually implement BaseCommandHandler, but currently does not
-  const ipc = new IPC(commandHandler as unknown as BaseCommandHandler, "chrome");
+  const ipc = new IPC(
+    (commandHandler as unknown) as BaseCommandHandler,
+    "chrome"
+  );
   ipc.start();
 });
