7491752e6f660aebef80b7115d68c9ebfdfad53d
cheng <ummcheng@gmail.com>
2020-04-28 18:32:56 -0700

    get scroll to to work-ish

--- src/navigation-handler.ts
+++ src/navigation-handler.ts
@@ -5,6 +5,19 @@ import CommandHandler from "./command-handler";
  */
 
 export default class NavigationHandler {
+  private static nodesMatching(path: string) {
+    return `
+      (function() { 
+        var snapshot = document.evaluate(".//*[not(self::script)][not(self::noscript)][not(self::title)][not(self::meta)][not(self::svg)][not(self::img)][not(self::style)][text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '${path}')]]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        var result = [];
+        for (var i = 0; i < snapshot.snapshotLength; i++) { 
+          result.push(snapshot.snapshotItem(i)); 
+        }
+        return result; 
+      })()
+    `;
+  }
+
   async COMMAND_TYPE_BACK(_data: any): Promise<any> {
     CommandHandler.executeScript("window.history.back();");
   }
@@ -18,6 +31,7 @@ export default class NavigationHandler {
   }
 
   async COMMAND_TYPE_SCROLL(data: any): Promise<any> {
+    // Scrolling in a direction
     let direction;
     switch (data.direction) {
       case "left":
@@ -36,6 +50,17 @@ export default class NavigationHandler {
     if (direction) {
       CommandHandler.executeScript(
         `window.scrollBy({ ${direction}, behavior: "smooth" });`
+      );
+    }
+
+    // Scrolling to a path
+    if (data.path) {
+      CommandHandler.executeScript(
+        `
+          ${NavigationHandler.nodesMatching(data.path)}[0].scrollIntoView({
+            block: "start", inline: "nearest", behavior: "smooth"
+          });
+        `
       );
     }
   }
