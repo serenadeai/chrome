8f3a7def9ad7216c72133483c1061b75dc542b91
cheng <ummcheng@gmail.com>
2020-05-01 13:52:29 -0700

    show icon if disconnected, open docs when icon clicked

--- src/extension.ts
+++ src/extension.ts
@@ -6,4 +6,19 @@ chrome.runtime.onInstalled.addListener(() => {
   const ipc = new IPC(commandHandler, "chrome");
   commandHandler.setIPC(ipc);
   ipc.start();
+
+  // If disconnected, show red badge
+  window.setInterval(() => {
+    if (!ipc.isConnected()) {
+      chrome.browserAction.setBadgeText({ text: "X" });
+      chrome.browserAction.setBadgeBackgroundColor({ color: "#a13a3a" });
+    } else {
+      chrome.browserAction.setBadgeText({ text: "" });
+    }
+  }, 1000);
+
+  chrome.browserAction.onClicked.addListener(() => {
+    const chromeDocs = "https://serenade.ai/docs";
+    chrome.tabs.create({ url: chromeDocs });
+  });
 });
