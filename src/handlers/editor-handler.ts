2dfa31d31cde63c5c25556e6c0fff55254f0deb6
cheng <ummcheng@gmail.com>
2020-10-19 23:14:50 -0700

    Set cursor to deleteEnd if inserting.

--- src/handlers/editor-handler.ts
+++ src/handlers/editor-handler.ts
@@ -74,9 +74,10 @@ export default class EditorHandler {
       // First try to set the cursor ourselves,
       // then tell the client to simulate key presses if needed.
       const cursor =
-        data.deleteEnd !== undefined &&
-        data.deleteStart !== undefined &&
-        data.deleteEnd - data.deleteStart !== 0
+        (data.deleteEnd !== undefined &&
+          data.deleteStart !== undefined &&
+          data.deleteEnd - data.deleteStart !== 0) ||
+        (data.insertDiff !== undefined && data.insertDiff !== "")
           ? data.deleteEnd
           : data.cursor;
 
