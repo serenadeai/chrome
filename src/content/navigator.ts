d5da49929511366d7dc191e1c7f6c2434fbbead6
cheng <ummcheng@gmail.com>
2020-12-08 14:27:03 -0800

    fix scrolling when in elements that have overflow and can't actually be scrolled, dynamic scroll distance

--- src/content/navigator.ts
+++ src/content/navigator.ts
@@ -1,52 +1,80 @@
 import Port = chrome.runtime.Port;
 
-const canScrollHorizonally = (element: Element) => {
-  return element.scrollWidth > element.clientWidth;
+const canScrollHorizontally = (element: Element) => {
+  const overflow = window.getComputedStyle(element).overflowX;
+  return (
+    element.scrollWidth > element.clientWidth && (overflow === "scroll" || overflow === "auto")
+  );
 };
 
 const canScrollVertically = (element: Element) => {
-  return element.scrollHeight > element.clientHeight;
+  const overflow = window.getComputedStyle(element).overflowY;
+  return (
+    element.scrollHeight > element.clientHeight && (overflow === "scroll" || overflow === "auto")
+  );
+};
+
+const scrollOptions = (element: Element | Window, direction: string) => {
+  let dir = {};
+  if ("clientWidth" in element) {
+    // Element type
+    switch (direction) {
+      case "left":
+        dir = { left: -element.clientWidth * 0.6 };
+        break;
+      case "right":
+        dir = { left: element.clientWidth * 0.6 };
+        break;
+      case "up":
+        dir = { top: -element.clientHeight * 0.6 };
+        break;
+      case "down":
+        dir = { top: element.clientHeight * 0.6 };
+    }
+  } else {
+    // Window type
+    switch (direction) {
+      case "left":
+        dir = { left: -element.innerWidth * 0.8 };
+        break;
+      case "right":
+        dir = { left: element.innerWidth * 0.8 };
+        break;
+      case "up":
+        dir = { top: -element.innerHeight * 0.8 };
+        break;
+      case "down":
+        dir = { top: element.innerHeight * 0.8 };
+    }
+  }
+
+  return Object.assign(dir, { behavior: "smooth" }) as ScrollOptions;
 };
 
 export const scrollDirection = (port: Port, data: { direction: string }) => {
-  let direction: ScrollToOptions | null = null;
-  switch (data.direction) {
-    case "left":
-      direction = { left: -window.innerWidth * 0.8 };
-      break;
-    case "right":
-      direction = { left: window.innerWidth * 0.8 };
-      break;
-    case "up":
-      direction = { top: -window.innerHeight * 0.8 };
-      break;
-    case "down":
-      direction = { top: window.innerHeight * 0.8 };
-      break;
-  }
-  if (direction) {
-    const horizontal = data.direction === "left" || data.direction === "right";
-    const vertical = data.direction === "up" || data.direction === "down";
-    const scrollOptions = Object.assign(direction, { behavior: "smooth" });
-
+  const horizontal = data.direction === "left" || data.direction === "right";
+  const vertical = data.direction === "up" || data.direction === "down";
+  // Quick validation
+  if (horizontal || vertical) {
     // Get the last hovered element, which should be the childmost node
     const elements = document.querySelectorAll("*:hover");
     let element = elements.length ? elements[elements.length - 1] : null;
-    if (element === null) {
-      // Fall back to scrolling window-wide
-      window.scrollBy(Object.assign(direction, { behavior: "smooth" }));
-    }
-    // Otherwise, try to scroll if possible, going up recursively
-    while (element) {
+    let scrolled = false;
+    // Try to scroll if possible, going up recursively
+    while (element && !scrolled) {
       if (
-        (horizontal && canScrollHorizonally(element)) ||
+        (horizontal && canScrollHorizontally(element)) ||
         (vertical && canScrollVertically(element))
       ) {
-        element.scrollBy(scrollOptions);
-        element = null;
+        element.scrollBy(scrollOptions(element, data.direction));
+        scrolled = true;
       } else {
         element = element.parentElement;
       }
+    }
+    if (!scrolled) {
+      // Fall back to scrolling window-wide
+      window.scrollBy(scrollOptions(window, data.direction));
     }
   }
   port.postMessage({ success: true });
