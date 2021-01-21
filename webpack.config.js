d585d2210069ad10267517fed8bc8fd104812077
cheng <ummcheng@gmail.com>
2021-01-20 16:25:48 -0800

    don't try to include a source map in production

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,36 @@
 
 const path = require("path");
 
-module.exports = [
-  {
-    name: "extension",
-    target: "web",
-    entry: {
-      extension: "./src/extension.ts",
-      page: "./src/content/page-command-handler.ts",
-    },
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].js"
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-      symlinks: false
-    },
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          exclude: /node_modules/,
-          test: /\.tsx?$/,
-          use: "ts-loader"
-        }
-      ]
-    }
+const config = {
+  name: "extension",
+  target: "web",
+  entry: {
+    extension: "./src/extension.ts",
+    page: "./src/content/page-command-handler.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    symlinks: false
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
   }
-];
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'eval';
+  }
+
+  return config;
+};
