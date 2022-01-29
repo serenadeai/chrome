47805bc7b2a9683d3eee6b7f66e312cf9411d706
Alexander Wulkan <alexanderwulkan@hotmail.com>
2022-01-29 02:28:44 +0100

    Change: Inject the script into the document's head

--- src/inject.js
+++ src/inject.js
@@ -1,14 +1,16 @@
 /**
- * injectScript - Inject internal script to available access to the `window`
+ * injectScript - Inject internal script into the document's head element
  *
- * @param  {type} file_path Local path of the internal script.
- * @param  {type} tag The tag as string, where the script will be append (default: 'body').
+ * @param  {string} filePath Local path of the internal script.
  * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
  */
-function injectScript(file_path) {
-  var script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", file_path);
-  document.documentElement.appendChild(script);
+function injectScript(filePath) {
+  const script = document.createElement("script");
+  script.type = 'text/javascript';
+  script.src = filePath;
+  (document.head || document.documentElement).appendChild(script);
 }
-injectScript(chrome.extension.getURL("build/page.js"));
+
+document.addEventListener("DOMContentLoaded", function() {
+  injectScript(chrome.extension.getURL("build/page.js"));
+});
