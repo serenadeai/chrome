30fa15ad8c556226f7b641ded0d0a028988297f1
Sam Dixon <sam.dixon@berkeley.edu>
2022-02-23 20:28:56 -0800

    maintain same IPC object

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,25 @@
 import ExtensionCommandHandler from "./extension-command-handler";
 import IPC from "./ipc";
 
-function connect() {
-  const extensionCommandHandler = new ExtensionCommandHandler();
-  const ipc = new IPC(
-    navigator.userAgent.indexOf("Brave") != -1
-      ? "brave"
-      : navigator.userAgent.indexOf("Edg") != -1
-        ? "edge"
-        : "chrome",
-    extensionCommandHandler
-  );
-  ipc.start();
-  return ipc;
-}
+const extensionCommandHandler = new ExtensionCommandHandler();
+const ipc = new IPC(
+  navigator.userAgent.indexOf("Brave") != -1
+    ? "brave"
+    : navigator.userAgent.indexOf("Edg") != -1
+      ? "edge"
+      : "chrome",
+  extensionCommandHandler
+);
+ipc.start();
 
-function keepAliveAndEnsureConnection() {
-  if (!ipc || !ipc.isConnected()) {
-    ipc = connect();
-  }
+function keepAlive() {
   chrome.alarms.create({ delayInMinutes: 2 });
 }
 
-let ipc = connect();
-chrome.alarms.onAlarm.addListener(keepAliveAndEnsureConnection);
+chrome.alarms.onAlarm.addListener(keepAlive);
 chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
   if (message.type === "reconnect") {
-    ipc = connect();
+    ipc.ensureConnection();
   }
 });
-keepAliveAndEnsureConnection();
+keepAlive();
